a8ad99ca57bb37ad07eb7c17e6bbd8321967433c
Graham Ballantyne <grahamb@sfu.ca>
2019-10-03 16:01:28 -0700

    Add loading spinner

--- public/js/rosterPhotos.js
+++ public/js/rosterPhotos.js
@@ -1,39 +1,20 @@
-const courseId = window.COURSE_ROSTER_PHOTOS.courseId;
+(async () => {
+  const hideSpinner = () => {
+    const spinner = document.getElementById('spinner');
+    spinner.classList += 'hidden';
+  };
+  try {
+    const courseId = window.COURSE_ROSTER_PHOTOS.courseId;
+    const response = await fetch(`/${courseId}`);
+    const responseBody = await response.text();
 
-fetch(`/${courseId}`)
-  .then(response => {
-    return response.text();
-  })
-  .then(html => {
+    hideSpinner();
     const el = document.getElementById('rosterGrid');
-    el.innerHTML = html;
-  })
-  .then(console.log);
-
-// $.ajax({
-//   url: url,
-//   beforeSend: function() {
-//     timer = window.setTimeout(showTimeoutMessage, 4000);
-//   },
-//   success: function(html) {
-//     clearTimeout(timer);
-//     $('.timeoutMessage').remove();
-//     spinner.stop();
-//     target.append(html);
-//     parent.postMessage(
-//       JSON.stringify({
-//         subject: 'lti.frameResize',
-//         height: $(document).height() + 'px',
-//       }),
-//       '*'
-//     );
-//   },
-//   error: function(xhr, status, error) {
-//     clearTimeout(timer);
-//     $('.timeoutMessage').remove();
-//     spinner.stop();
-//     target.append(
-//       '<p>Sorry, there was a problem retrieving the roster photos for this course. If this issue presists, please quit your browser and try again.</p>'
-//     );
-//   },
-// });
+    el.innerHTML = responseBody;
+  } catch (error) {
+    hideSpinner();
+    const el = document.getElementById('error');
+    el.innerHTML =
+      '<p>Sorry, there was a problem retrieving the roster photos for this course. If this issue presists, please quit your browser and try again.</p>';
+  }
+})();
